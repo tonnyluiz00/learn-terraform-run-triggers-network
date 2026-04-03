cf4cd67001deefd251eae5481f7e16b166be7fe9
Tonny Luiz <tonnyluiz00@gmail.com>
2026-04-03 16:05:33 -0300

    Update Terraform remote state configuration

--- main.tf
+++ main.tf
@@ -272,9 +272,9 @@ data "terraform_remote_state" "network" {
   backend = "remote"
 
   config = {
-    organization = var.tfc_org_name
+    organization = "tonnyluiz-terraform"
     workspaces = {
-          name = var.tfc_network_workspace_name
+          name = "learn-terraform-run-triggers-network"
     }
   }
 }
